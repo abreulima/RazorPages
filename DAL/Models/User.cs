8b171adcfbeb14690962d9b1bc1adbede01cc81d
abreulima <ivan.rupt@gmail.com>
2026-01-27 19:35:44 +0000

    Updating database.

--- DAL/Models/User.cs
+++ DAL/Models/User.cs
@@ -5,7 +5,7 @@ namespace DAL.Models
         public int      Id { get; set; }
         public string   Name { get; set; }
         public string   Email { get; set; }
-        public String Password { get; set; }
+        public string Password { get; set; }
         public DateTime CreationDate { get; set; }
     }
 }
